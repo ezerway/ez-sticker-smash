49b21dba6414c686ea3fbeeeef15928589f19f57
ezerway <54234094+ezerway@users.noreply.github.com>
2024-03-28 23:39:37 +0700

    update admin

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
     },
+    publicPath: process.env.NODE_ENV === "production" ? "/admin/" : "/",
     transpileDependencies: ["vuetify"],
     pluginOptions: {
         i18n: {
